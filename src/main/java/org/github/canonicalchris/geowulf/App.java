7e3eae64c5a6fe7d3e20db32e0420bcd7731f3ed
Robert C. Kahlert <Robert.Kahlert@gmail.com>
2013-08-11 07:42:37 -0500

    put basic project in place

---
+++ src/main/java/org/github/canonicalchris/geowulf/App.java
@@ -0,0 +1,13 @@
+package org.github.canonicalchris.geowulf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
